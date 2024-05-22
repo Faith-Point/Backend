366e25e2aa277829b08bf8f8018ef02a536f5303
Lucas Davi Ferreira de Faria <lucas.faria1@gmail.com>
2024-05-22 17:08:39 -0300

    feat: add new configs in docker and workflows

--- Dockerfile
+++ Dockerfile
@@ -6,18 +6,19 @@ RUN apk add --no-cache python3 make g++ && \
     npm install -g yarn --force
 
 COPY package.json yarn.lock tsconfig.json ./
-
 RUN yarn install --frozen-lockfile
 
 COPY src /app/src
 COPY ormconfig.json /app/ormconfig.json
 COPY src/config/init-db.sh /app/src/config/init-db.sh
 COPY src/config/data-source.ts /app/src/config/data-source.ts
+COPY src/config/wait-for-it.sh /app/wait-for-it.sh
 
 RUN chmod +x /app/src/config/init-db.sh
+RUN chmod +x /app/src/config/wait-for-it.sh
 
 RUN yarn build
 
 EXPOSE 3308
 
-CMD ["sh", "-c", "yarn typeorm migration:run -d /app/src/config/data-source.ts && yarn start"]
+CMD ["sh", "-c", "/app/src/config/wait-for-it.sh faith-point && yarn typeorm migration:run -d /app/src/config/data-source.ts && yarn start"]
--- src/config/swaggerComponents.ts
+++ src/config/swaggerComponents.ts
@@ -34,216 +34,216 @@ export const swaggerComponents = {
             type: 'string',
             format: 'date-time',
           },
+        },        
+      },
+      State:{
+        type: 'object',
+        properties: {
+          id: {
+            type: 'string',
+            format: 'uuid',
+          },
+          short_name: {
+            type: 'string',
+            maxLength: 2,
+          },
+          long_name: {
+            type: 'string',
+            maxLength: 32,
+          },
+          code: {
+            type: 'string',
+            maxLength: 32,
+          },
+          country_id: {
+           $ref: '#/components/schemas/Country',
+          },
+          created_at: {
+            type: 'string',
+            format: 'date-time',
+          },
+          updated_at: {
+            type: 'string',
+            format: 'date-time',
+          },
+          deleted_at: {
+            type: 'string',
+            format: 'date-time',
+          },
+        },        
+      },
+      City: {
+        type: 'object',
+        properties: {
+          id: {
+            type: 'string',
+            format: 'uuid',
+          },
+          short_name: {
+            type: 'string',
+            maxLength: 2,
+          },
+          long_name: {
+            type: 'string',
+            maxLength: 32,
+          },
+          code: {
+            type: 'string',
+            maxLength: 32,
+          },
+          state_id: {
+            $ref: '#/components/schemas/State',
+          },
+          created_at: {
+            type: 'string',
+            format: 'date-time',
+          },
+          updated_at: {
+            type: 'string',
+            format: 'date-time',
+          },
+          deleted_at: {
+            type: 'string',
+            format: 'date-time',
+          },
         },
-        State:{
-          type: 'object',
-          properties: {
-            id: {
-              type: 'string',
-              format: 'uuid',
-            },
-            short_name: {
-              type: 'string',
-              maxLength: 2,
-            },
-            long_name: {
-              type: 'string',
-              maxLength: 32,
-            },
-            code: {
-              type: 'string',
-              maxLength: 32,
-            },
-            country_id: {
-             $ref: '#/components/schemas/Country',
-            },
-            created_at: {
-              type: 'string',
-              format: 'date-time',
-            },
-            updated_at: {
-              type: 'string',
-              format: 'date-time',
-            },
-            deleted_at: {
-              type: 'string',
-              format: 'date-time',
-            },
+      },
+      Address: {
+        type: 'object',
+        properties: {
+          id: {
+            type: 'string',
+            format: 'uuid',
           },
-          City: {
-            type: 'object',
-            properties: {
-              id: {
-                type: 'string',
-                format: 'uuid',
-              },
-              short_name: {
-                type: 'string',
-                maxLength: 2,
-              },
-              long_name: {
-                type: 'string',
-                maxLength: 32,
-              },
-              code: {
-                type: 'string',
-                maxLength: 32,
-              },
-              state_id: {
-                $ref: '#/components/schemas/State',
-              },
-              created_at: {
-                type: 'string',
-                format: 'date-time',
-              },
-              updated_at: {
-                type: 'string',
-                format: 'date-time',
-              },
-              deleted_at: {
-                type: 'string',
-                format: 'date-time',
-              },
-            },
+          street: {
+            type: 'string',
+            maxLength: 255,
           },
-          Address: {
-            type: 'object',
-            properties: {
-              id: {
-                type: 'string',
-                format: 'uuid',
-              },
-              street: {
-                type: 'string',
-                maxLength: 255,
-              },
-              number: {
-                type: 'string',
-                maxLength: 32,
-              },
-              complement: {
-                type: 'string',
-                maxLength: 255,
-              },
-              neighborhood: {
-                type: 'string',
-                maxLength: 255,
-              },
-              city_id: {
-                $ref: '#/components/schemas/City',
-              },
-              created_at: {
-                type: 'string',
-                format: 'date-time',
-              },
-              updated_at: {
-                type: 'string',
-                format: 'date-time',
-              },
-              deleted_at: {
-                type: 'string',
-                format: 'date-time',
-              },
-            },
-            Role: {
-              type: 'object',
-              properties: {
-                id: {
-                  type: 'string',
-                  format: 'uuid',
-                },
-                name: {
-                  type: 'string',
-                  maxLength: 32,
-                },
-                description: {
-                  type: 'string',
-                  maxLength: 255,
-                },
-                created_at: {
-                  type: 'string',
-                  format: 'date-time',
-                },
-                updated_at: {
-                  type: 'string',
-                  format: 'date-time',
-                },
-                deleted_at: {
-                  type: 'string',
-                  format: 'date-time',
-                },
-              },
-              User: {
-                type: 'object',
-                properties: {
-                  id: {
-                    type: 'string',
-                    format: 'uuid',
-                  },
-                  name: {
-                    type: 'string',
-                    maxLength: 255,
-                  },
-                  email: {
-                    type: 'string',
-                    format: 'email',
-                    maxLength: 255,
-                  },
-                  password: {
-                    type: 'string',
-                    maxLength: 255,
-                  },
-                  role_id: {
-                    $ref: '#/components/schemas/Role',
-                  },
-                  address_id: {
-                    $ref: '#/components/schemas/Address',
-                  },
-                  created_at: {
-                    type: 'string',
-                    format: 'date-time',
-                  },
-                  updated_at: {
-                    type: 'string',
-                    format: 'date-time',
-                  },
-                  deleted_at: {
-                    type: 'string',
-                    format: 'date-time',
-                  },
-                },
-                LogAuth: {
-                  type: 'object',
-                  properties: {
-                    id: {
-                      type: 'string',
-                      format: 'uuid',
-                    },
-                    user_id: {
-                      $ref: '#/components/schemas/User',
-                    },
-                    token: {
-                      type: 'string',
-                      maxLength: 255,
-                    },
-                    created_at: {
-                      type: 'string',
-                      format: 'date-time',
-                    },
-                    updated_at: {
-                      type: 'string',
-                      format: 'date-time',
-                    },
-                    deleted_at: {
-                      type: 'string',
-                      format: 'date-time',
-                    },
-                  },
-                }
-              }
-            }
-          }
-        }
+          number: {
+            type: 'string',
+            maxLength: 32,
+          },
+          complement: {
+            type: 'string',
+            maxLength: 255,
+          },
+          neighborhood: {
+            type: 'string',
+            maxLength: 255,
+          },
+          city_id: {
+            $ref: '#/components/schemas/City',
+          },
+          created_at: {
+            type: 'string',
+            format: 'date-time',
+          },
+          updated_at: {
+            type: 'string',
+            format: 'date-time',
+          },
+          deleted_at: {
+            type: 'string',
+            format: 'date-time',
+          },
+        },        
+      },
+      Role: {
+        type: 'object',
+        properties: {
+          id: {
+            type: 'string',
+            format: 'uuid',
+          },
+          name: {
+            type: 'string',
+            maxLength: 32,
+          },
+          description: {
+            type: 'string',
+            maxLength: 255,
+          },
+          created_at: {
+            type: 'string',
+            format: 'date-time',
+          },
+          updated_at: {
+            type: 'string',
+            format: 'date-time',
+          },
+          deleted_at: {
+            type: 'string',
+            format: 'date-time',
+          },
+        },        
+      },
+      User: {
+        type: 'object',
+        properties: {
+          id: {
+            type: 'string',
+            format: 'uuid',
+          },
+          name: {
+            type: 'string',
+            maxLength: 255,
+          },
+          email: {
+            type: 'string',
+            format: 'email',
+            maxLength: 255,
+          },
+          password: {
+            type: 'string',
+            maxLength: 255,
+          },
+          role_id: {
+            $ref: '#/components/schemas/Role',
+          },
+          address_id: {
+            $ref: '#/components/schemas/Address',
+          },
+          created_at: {
+            type: 'string',
+            format: 'date-time',
+          },
+          updated_at: {
+            type: 'string',
+            format: 'date-time',
+          },
+          deleted_at: {
+            type: 'string',
+            format: 'date-time',
+          },
+        },        
+      },
+      LogAuth: {
+        type: 'object',
+        properties: {
+          id: {
+            type: 'string',
+            format: 'uuid',
+          },
+          user_id: {
+            $ref: '#/components/schemas/User',
+          },
+          token: {
+            type: 'string',
+            maxLength: 255,
+          },
+          created_at: {
+            type: 'string',
+            format: 'date-time',
+          },
+          updated_at: {
+            type: 'string',
+            format: 'date-time',
+          },
+          deleted_at: {
+            type: 'string',
+            format: 'date-time',
+          },
+        },
       }
     },
   },
