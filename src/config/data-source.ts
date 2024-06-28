8ac99c916fefa899208cb3ce4860a9d51c5b08be
Lucas Davi Ferreira de Faria <lucas.faria1@gmail.com>
2024-06-28 20:17:27 -0300

    fix: testing production configuration

--- src/config/data-source.ts
+++ src/config/data-source.ts
@@ -1,15 +1,18 @@
 import 'reflect-metadata';
 import { DataSource } from 'typeorm';
 import ormConfig from '../../ormconfig.json';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const AppDataSource = new DataSource({
   ...ormConfig,
   type: 'postgres',
-  host: process.env.DB_HOST || ormConfig.host,
-  port: parseInt(process.env.DB_PORT || ormConfig.port.toString(), 10),
-  username: process.env.DB_USERNAME || ormConfig.username,
-  password: process.env.DB_PASSWORD || ormConfig.password,
-  database: process.env.DB_NAME || ormConfig.database,
+  host: process.env.DB_HOST,
+  port: parseInt(process.env.DB_PORT || '', 10),
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
 });
 
 export const initializeDataSource = async () => {
