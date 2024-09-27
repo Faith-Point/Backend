3e3466dd4585e85e86858a276e2e89fa5330b9cb
Lucas Davi Ferreira de Faria <lucas.faria1@gmail.com>
2024-09-27 11:45:45 -0300

    fix: limit to using less memory

--- src/shared/http/app.ts
+++ src/shared/http/app.ts
@@ -6,16 +6,18 @@ import 'es6-shim';
 import express from 'express';
 import setupRoutes from './routes';
 import { container } from 'tsyringe';
-import { initializeDataSource } from '@config/data-source'; // Importando a função de inicialização do Data Source
+import { initializeDataSource } from '@config/data-source';
 import Handler from '@shared/exceptions/Handler';
 import ShowPrettyError from '@shared/exceptions/ShowPrettyError';
 import CreateLogExceptionService from '@modules/shared/logException/services/CreateLogExceptionService';
 import ErrorResponse from '@shared/http/response/ErrorResponse';
 import http from '@config/http';
 import setupSwagger from '@config/swagger';
+import compression from 'compression';
 
 const app = express();
 
+app.use(compression());
 app.use(express.json());
 
 initializeDataSource().then(() => {
@@ -23,16 +25,16 @@ initializeDataSource().then(() => {
   app.use(setupRoutes);
 
   app.use(async (error: Handler, request: express.Request, response: express.Response) => {
-      let handler = error;
+    let handler = error;
 
-      ShowPrettyError.execute(error);
+    ShowPrettyError.execute(error);
 
-      handler.statusCode = typeof handler.statusCode === 'undefined' ? http.INTERNAL_SERVER_ERROR : handler.statusCode;
+    handler.statusCode = typeof handler.statusCode === 'undefined' ? http.INTERNAL_SERVER_ERROR : handler.statusCode;
 
-      const createLogExceptionService = container.resolve(CreateLogExceptionService);
-      createLogExceptionService.execute(handler, request);
+    const createLogExceptionService = container.resolve(CreateLogExceptionService);
+    createLogExceptionService.execute(handler, request);
 
-      return response.status(handler.statusCode).json(new ErrorResponse().execute(handler));
+    return response.status(handler.statusCode).json(new ErrorResponse().execute(handler));
   });
 }).catch(error => {
   console.error('Failed to initialize the database connection:', error);
